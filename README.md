4162014ffcdca88d4b7e005b3c4a2ab107a7208f
Cassia Carvalho <contato.cassia.carvalho@gmail.com>
2023-03-23 00:35:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# dicionario-escritoras-brasileiras
+## Desafio final do curso EuProgramo Front-End II da PrograMaria
+
+💻 O Projeto
+É um dicionário autoral, que lista as escritoras brasileiras que fizeram e ainda fazem parte da história do Brasil e do mundo. 
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML
+- CSS
+- JavaScript
+
+- [Visite o projeto online]()
---
+++ script.js
@@ -0,0 +1,87 @@
+function carregaDicionario() {
+    var biografias = {
+
+        bio01: {
+            nome: "Carolina Maria de Jesus",
+            imagem: "https://www.socialismocriativo.com.br/wp-content/uploads/2021/02/Carolina-Maria-de-Jesus.jpeg",
+            descricao: "Carolina Maria de Jesus é uma mulher negra, pobre e catadora de papel de uma favela de São Paulo, foi uma das primeiras escritoras negras no Brasil e é considerada uma das mais importantes escritoras brasileiras negras do país. Como forma de manifesto social, Carolina escrevia para fugir da sua realidade, escrevia críticas sociais e registrava a realidade da favela. Ou seja, a fome, o racismo e a invisibilidade social em seus diários. Carolina não teve uma vida fácil, durante o dia ela escrevia e a noite ela trabalhava como catadora de papel e guardava para ler todas as revistas que encontrava. Sua história de luta foi descoberta pelo jornalista Audálio Dantas e seus escritos foram transformados no livro Quarto de Despejo: Diário de uma Favelada (1960), que se tornou um best-seller traduzido para 14 idiomas.",
+            citacao: "As crianças ricas brincam nos jardins com seus brinquedos prediletos. E as crianças pobres acompanham as mães a pedirem esmolas pelas ruas. Que desigualdades trágicas e que brincadeira do destino."},
+         bio02: {
+            nome: "Cecilia Meireles",
+            imagem: "https://lirp.cdn-website.com/174487e2/dms3rep/multi/opt/cecilia-e1559743931230-640w.jpg",
+            descricao: "A carioca Cecília Meireles é considerada uma das poetisas mais importantes da literatura nacional e pertence à segunda fase do Modernismo Brasileiro. Cecília Meireles, codinome de Cecília Benevides de Carvalho Meireles, foi poeta, ensaísta, cronista, folclorista, tradutora, educadora e pintora. Ganhadora de vários prêmios, Prêmio de Tradução/Teatro, concedido pela Associação Paulista de Críticos de Arte, em 1962; em 1963 o Prêmio Jabuti de Tradução de Obra Literária, pelo livro Poemas de Israel, concedido pela Câmara Brasileira do Livro; em 1964 recebeu o Jabuti de poesia pelo livro Solombra; e em 1965, o Prêmio Machado de Assis, da Academia Brasileira de Letras, pelo conjunto de sua obra, Cecília escreveu poemas intimistas que, influenciados pela psicanálise, abordam questões psicológicas e espirituais. Ela também se destacou na literatura infantil.",
+            citacao: "Liberdade é uma palavra que o sonho humano alimenta, não há ninguém que explique e ninguém que não entenda.",
+            },
+        bio03: {
+            nome: "Clarice Lispector",
+            imagem: "https://www.araguari.mg.leg.br/imagens/clarice-lispector.jpg/image_preview",
+            descricao: "Nascida na Ucrânia e naturalizada no Brasil, ela se autointitulada como pernambucana. E só isso já diz muito sobre sua personalidade! Mas além disso, Clarice foi uma grande escritora e jornalista brasileira, autora de vários romances, contos e ensaios. Ela é reconhecida como uma das escritoras mais importantes do século XX e a maior escritora judia desde Franz Kafka. Clarice se destacou na fase do Modernismo Brasileiro, onde colocou em dúvida os modelos narrativos tradicionais com seus romances inovadores, com linguagem altamente poética, enigmática e introspectiva. Umas das características das obras de Clarice Lispector é retratar cenas do cotidiano. Porém, ela prende a atenção do leitor com diversos dilemas existenciais de suas personagens e utilizando intensos fluxos de pensamentos. Portanto, é bastante compreensivo do porquê ela é considerada uma das grandes escritoras brasileiras, né? Mas, ela também recebeu vários prêmios como, por exemplo, o Prêmio Carmem Dolores Barbosa (1961), o Prêmio Graça Aranha (1943) e o Prêmio Jabuti (1961, 1978).",
+            citacao: "O medo sempre me guiou para o que eu quero. E porque eu quero, temo. Muitas vezes foi o medo que me tomou pela mão e me levou. O medo me leva ao perigo. E tudo o que eu amo é arriscado."
+            },
+        bio04: {
+            nome: "Conceição Evaristo",
+            imagem: "https://ea9vhhuzko5.exactdn.com/wp-content/uploads/2019/09/image-2.jpg?strip=all&lossy=1&ssl=1",
+            descricao: "A doutora Maria Conceição Evaristo, nasceu em família pobre, é segunda filha de 9 irmãos, sendo a primeira da sua casa a conseguir um diploma universitário. Em 1996, Conceição defendeu seu trabalho de conclusão de mestrado em Literatura Brasileira na Pontifícia, na Universidade Católica do Rio de Janeiro (PUC/RJ). O tema da sua dissertação teve como título a Literatura Negra: uma poética da nossa afro-brasilidade. Com todo o seu empenho, ela também concluiu seu doutorado em 2011, com a tese “Poemas Malungos – Cânticos Irmãos”, na Universidade Federal Fluminense (UFF). Em suas obras, poesia e prosa, Conceição retrata a vivência das mulheres negras no Brasil, abordando temas como a discriminação racial, violência urbana, discriminação de gênero e de classe e resistência, e a ancestralidade africana. Sendo assim, entre seus livros mais famosos, temos o livro “Olhos d’água” que foi vencedor do Prêmio Jabuti em 2017. A obra reúne 15 contos que abordam a miséria, desigualdade social e os dilemas sobre o amor, a vida e a ancestralidade africana.",
+            citacao: "Minha escrita é contaminada pela condição de mulher negra",
+        },
+        bio05: {
+            nome: "Cora Coralina",
+            imagem: "https://nordestinadosaler.com.br/wp-content/uploads/2021/08/cora.png",
+            descricao: "Poetisa e contista brasileira publicou seu primeiro livro aos 75 anos, tornando-se uma das melhores vozes femininas. Além de ser considerada como uma das escritoras mais importantes no mundo literário nacional. Ana Lins dos Guimarães Peixoto é o pseudônimo utilizado por Cora, sendo o destaque da poesia do estado de Goiás. E apesar de ela ter começado a escrever ainda na adolescência, Cora só conseguiu realizar o sonho de publicar seu primeiro livro “O Poema dos Becos de Goiás e Estórias Mais” em 1965.  Seu primeiro livro foi considerado como uma das obras mais importantes do século XX, pelo jornal O Popular, de Goiânia. E em 1970, Cora Carolina passa a ser uma das poucas mulheres a ter posse de uma cadeira na Academia Feminina de Letras e Artes de Goiás.  Já em 1976, ela lança seu segundo livro “Meu Livro de Cordel”, ao qual recebeu muitos elogios do grande poeta Carlos Drummond de Andrade, em 1980.  Suas obras retratam sua vida simples no interior, além de falar sobre o tempo, futuro e a realidade das mulheres em 1900. Seus versos singelos, delicados e muito profundos conquistaram o prêmio Juca Pato, da União Brasileira dos Escritores e em 1984, Cora foi nomeada para a Academia Goiana de Letras, ocupando a cadeira nº. 38.",
+            citacao: "Eu sou aquela mulher que fez a escalada da montanha da vida, removendo pedras e plantando flores.",
+        },
+        bio06: {
+            nome: "Eliana Alves Cruz",
+            imagem: "https://imagens.ne10.uol.com.br/veiculos/_midias/jpg/2020/08/19/eliana_alvez_cruz_divulgacao04-16531761.jpg",
+            descricao: "Chefe do Departamento de Imprensa da Confederação Brasileira de Esportes Aquáticos e vice-presidente do Comitê de Mídia da Federação Internacional de Natação – FINA, Eliana Alves Cruz também se destaca pela sua produção literária, com livros como o romance Água de barrela, resultado de cinco anos de pesquisa sobre a história de sua família, desde os tempos da escravidão. A publicação foi contemplada em primeiro lugar no Prêmio Oliveira Silveira 2015, concurso promovido pela Fundação Cultural Palmares. Lançado em 2022, o livro Solitária aborda a história de duas mulheres negras, Eunice e Mabel, mãe e filha que moram na casa na qual a mãe trabalha como empregada, em um condomínio de luxo. Eunice, a mãe, é testemunha-chave de um crime chocante ocorrido na casa dos patrões. Mabel, a filha, constrói o caminho que leva não apenas à elucidação deste crime, mas a uma mudança radical na vida das pessoas que cercam as protagonistas.",
+            citacao: "Ouvia pessoas apontando rancores. Gostavam de colocar palavras em sua boca e pensamentos em sua cabeça.",
+        },
+        bio07: {
+            nome: "Hilda Hilst",
+            imagem: "http://2.bp.blogspot.com/-6SsyxIVvnV4/UvDZa6r0cUI/AAAAAAAAFe8/FIc0miAM4fQ/s1600/Picture+2.png",
+            descricao: "Considerada uma das grandes escritoras em língua portuguesa do século XX, Hilda Hilst foi uma grande poetisa, cronista, dramaturga e ficcionista brasileira. Seus livros e poemas tinham como características abordar o universo feminino com seus desejos e sentimentos. De forma mais clara, Hilda abordava sobre o amor, erotismo, sexualidade, moral e costumes, entre outros temas que eram considerados “polêmicos” para sua época. Por isso, é inegociável que Hilda era uma mulher muito à frente do seu tempo e que enfrentou os preconceitos da sociedade para publicar suas obras. Hilda Hilst fez parte da geração de 45, ou seja, terceira geração Modernista, em que escritores buscavam a reabilitação de regras mais rígidas para a composição dos versos. Além disso, o seu primeiro livro de poesia foi publicado em 1950, intitulado como “Presságio”. Hilda foi muito premiada por suas obras, ganhou o Prêmio APCA de melhor livro do ano, em 1977, com o livro Ficções de Hilda Hilst. E em 1981 ganhou o Grande Prêmio da Crítica pelo Conjunto da Obra – Associação Paulista de Críticos de Arte.",
+            citacao: "Ama-me, é tempo ainda.",
+        },
+        bio08: {
+            nome: "Lygia Fagundes Telles",
+            imagem: "https://teoriaedebate.org.br/wp-content/uploads/sites/2/2022/04/Captura-de-Tela-2022-04-06-as-12.14.49.png",
+            descricao: "Grande escritora paulistana, romancista e contista, que cresceu rodeada de histórias de terror e de personagens folclóricos. Em 1938, Lydia publicou seu primeiro livro de contos, intitulado de “Porão e Sobrado”, onde seu pai financiou a publicação da obra, ou seja, a sua estreia no mundo literário. Porém, não precisou de muito tempo para que Lydia se tornasse uma escritora famosa e reconhecida no Brasil e internacionalmente. Portanto, Lygia Fagundes é uma grande representante do movimento pós-modernismo e suas obras ganharam inúmeros prêmios. O romance, “As meninas”, publicado em 1973, ganhou alguns prêmios, entre eles, o prêmio Jabuti e o Coelho Neto, da Academia Brasileira de Letras. Também é uma das poucas mulheres que conseguiu ocupar uma cadeira na Academia Brasileira de Letras, e ser membro da Academia Paulista de Letras, e da Academia de Ciências de Lisboa. Suas obras retratam temas clássicos e discutem questões existenciais, como a condição da mulher na sociedade, além de temas como o amor, a morte, a loucura, a fantasia e o medo. ",
+            citacao: "Não peça coerência ao mistério nem peça lógica ao absurdo.",
+        },
+        bio09: {
+            nome: "Maria Firmina dos Reis",
+            imagem: "https://static.dw.com/image/63149551_804.png",
+            descricao: "Filha bastarda de uma mulher escravizada, Maria Firmina dos Reis (1822-1917) é considerada a primeira romancista negra brasileira. Seu romance, Úrsula, publicado pela primeira vez em 1859, é também um precursor da literatura antiescravista e da produção literária negra no Brasil, ou seja, da literatura que aborda o tema da negritude a partir da perspectiva do próprio negro. Obra e autora ficaram por muitos anos esquecidos e apagados do cânone nacional: embora Maria Firmina tenha recebido homenagens à época de sua publicação, estas não alçaram, até hoje, os devidos reconhecimentos e méritos. Dona de uma primorosa expressão literária e de uma biografia comprometida com a luta por direitos e equidade educacional, Maria Firmina dos Reis é uma grande pioneira na literatura e na história nacionais.",
+            citacao: "Eu a vi - gentil mimosa, Os lábios da cor da rosa, A voz um hino de amor! Eu a vi, lânguida, e bela: E ele a rever-se nela: Ele colibri - ela flor.",
+        },
+        bio10: {
+            nome: "Rachel de Queiroz",
+            imagem: "https://www.todoestudo.com.br/wp-content/uploads/2019/09/rachel-de-queiroz-2-1-e1569171762702.png",
+            descricao: "Rachel de Queiroz também foi uma grande escritora brasileira, romancista, tradutora, jornalista, teatróloga cearense e importante dramaturga brasileira. Mas, ela também foi a primeira mulher a entrar para a Academia Brasileira de Letras (ABL) em 1994. Sim, estamos falando de uma mulher conquistadora, mas ela não parou por aí! O seu primeiro romance “O Quinze”, conquistou o prêmio da Fundação Graça Aranha em 1930, e Rachel também foi a primeira mulher a receber o Prêmio Camões em 1993. Essa grande escritora brasileira pertence à segunda geração modernista e foi com o seu primeiro romance “O Quinze” que ela conquistou seu reconhecimento. O livro é uma obra de fundo social, que retrata a dramática realidade que os retirantes nordestinos estavam submetidos durante a seca que assolou o Nordeste em 1915.",
+            citacao: "Fala-se muito na crueldade e na bruteza do homem medievo. Mas o homem moderno será melhor?",
+        },
+};
+
+var content = document.getElementById("content");
+
+for (var bio in biografias) {
+    content.innerHTML +=
+    '<div class="card">' +
+    '<img src="' +
+    biografias[bio].imagem +
+    '"/>' +
+    "<details>" +
+    "<summary>" +
+    biografias[bio].nome +
+    "</summary>" +
+    "<p>" +
+    biografias[bio].descricao +
+    "</p>" +
+    "<blockquote><q>" +
+    biografias[bio].citacao +
+    "</q></blockquote>" +
+    "</details>" +
+    "</div>";
+}
+}
+carregaDicionario();
